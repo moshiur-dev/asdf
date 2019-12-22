645d26113355144f493c34c96f79f8b31b3ca944
moshiurfr <moshiurr882@gmail.com>
2019-12-22 11:47:59 +0600

    all file added

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+/*$(document) .ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:2,
+        autoplay: true,
+        autoplayTimeout: .5s,
+        autoplayHoverPause: true,
+        loop: true,
+        dots: false,
+        nav: true,
+        navText: ["<i class='fa fa-long-arrow-left'></i>", "<i class='fa fa-long-arrow-right'></i>"]
+    });
+});
+*/
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:3,
+        autoplay: true,
+    });
+  });
